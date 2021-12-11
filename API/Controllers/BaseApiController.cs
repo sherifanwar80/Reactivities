7315386ceabeb8f721e2c1000ebe782cbfd0a94d
sherifanwar80 <sherifanwar@yahoo.com>
2021-12-11 23:54:51 +0200

    End of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 //this function created to inherit from not to repeat the attributes [ApiController]/[Route("[controller]")]
 namespace API.Controllers
@@ -8,5 +10,8 @@ namespace API.Controllers
     public class BaseApiController : ControllerBase
     {
         
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        // protected IMediator Mediator => HttpContext.RequestServices.GetService<IMediator>();
     }
 }
